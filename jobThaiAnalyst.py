59dc34e2c5ebdee673eb8df668912e4779c3f3dd
GnipN <nattapon.nr@gmail.com>
2021-09-16 19:39:29 +0700

    add find by custom word

--- jobThaiAnalyst.py
+++ jobThaiAnalyst.py
@@ -1,6 +1,54 @@
 from connectMongo import get_database
+from pymongo import MongoClient
+import pymongo
 
+# show all by sorting
 # connect db
-db = get_database()
-collectionJobTitleWord = db["jobTitleWord"]
+def getAllWord(limitRow):
+    db = get_database() 
+    collectionJobTitleWordCount = db["jobTitleWordCount"]
+    collectionNoMeaningWord = db["noMeaningWord"]
+
+    results = collectionJobTitleWordCount.find().sort("count",pymongo.DESCENDING).limit(limitRow)
+    row = 1
+    for result in results:
+        # skip no meaning word
+        if collectionNoMeaningWord.count_documents({"word":result['word']}) == 0:
+            print(row, " ", end=" ")
+            print(result['word']," ", result["count"])
+        row = row + 1
+
+def getJob(jobStr):
+    db = get_database() 
+    collectionJobTitleWordCount = db["jobTitleWordCount"]
+    collectionSimilarWord = db["similarWord"]
+
+    # get similar word for this job
+    simWords = collectionSimilarWord.find({"mean":jobStr},{'simWord':1,'_id':0})
+    # convert key to word
+    simWordList = []
+    for w in simWords:
+        simWordList.append({'word':w['simWord']})
+    # count all jobs
+    countJobs = 0
+    jobAllRows = collectionJobTitleWordCount.find({'$or':simWordList},{'count':1,'_id':0})
+    for r in jobAllRows:
+        countJobs = countJobs + r['count']
+    return countJobs
+
+def getAllCustomWord():
+    db = get_database() 
+    collectionJobTitleWordCount = db["jobTitleWordCount"]
+    collectionSimilarWord = db["similarWord"]
+
+    # all custom words
+    means = collectionSimilarWord.distinct( "mean" )
+    for m in means:
+        print(m ,' = ', getJob(m)  )
+        #print(m)
+
+# print('dataAnalyst = ', getJob('dataAnalyst')  )
+# print('programmer = ', getJob('programmer')  )
+# getAllWord(50)
+getAllCustomWord()
 
