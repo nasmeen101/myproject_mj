1bc159d50eead739927e9ce483924c26e43c8bfa
GnipN <nattapon.nr@gmail.com>
2021-09-18 09:53:34 +0700

    test change to https

--- mongo/install-mongodb.sh
+++ mongo/install-mongodb.sh
@@ -1,7 +1,7 @@
 sudo wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
 sudo apt-get install gnupg
 sudo wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-sudo echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo echo "deb https://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 sudo apt-get update
 yes Y | sudo apt-get install mongodb-org
 sudo systemctl start mongod
