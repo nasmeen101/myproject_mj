e63164fb26d1653d741774e4f4f75546bcca26d0
nasmeen101 <nasmeen000@gmail.com>
2022-12-02 11:19:04 +0700

    first commit

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from wordCount import WordCount
 from wordTokenize import WordTokenize
 from getJobs import GetJobs
 
-# gt = GetJobs()
-# retrunCode = gt.getJobThai('jouThaiUsr01','jobpass','jobThai','jobList')
+gt = GetJobs()
+retrunCode = gt.getJobThai('jouThaiUsr01','jobpass','jobThai','jobList')
 
 wt = WordTokenize()
 wt.segWord('jouThaiUsr01','jobpass','jobThai','jobList' ,'jobTitle'
@@ -15,19 +15,19 @@ wt.segWord('jouThaiUsr01','jobpass','jobThai','jobList' ,'jobDetail'
 wt.segWord('jouThaiUsr01','jobpass','jobThai','jobList' ,'jobRequire'
                                                         ,'jobRequireWord'
                                                         ,{'lastAct' : 'seg_jobDetail'})
-# wt.segWord('jouThaiUsr01','jobpass','jobThai','jobList' ,'jobDetail'
-#                                                         ,'jobDetailWord'
-#                                                         ,{"$or":[   {'lastAct' : 'newAdded'}, 
-#                                                                     {'lastAct' : 'seg_jobTitle'}
-#                                                                 ]})
-# wt.segWord('jouThaiUsr01','jobpass','jobThai','jobList' ,'jobRequire'
-#                                                         ,'jobRequireWord'
-#                                                         ,{"$or":[   {'lastAct' : 'newAdded'}, 
-#                                                                     {'lastAct' : 'seg_jobTitle'},
-#                                                                     {'lastAct' : 'seg_jobDetail'}
-#                                                                 ]})
+wt.segWord('jouThaiUsr01','jobpass','jobThai','jobList' ,'jobDetail'
+                                                        ,'jobDetailWord'
+                                                        ,{"$or":[   {'lastAct' : 'newAdded'}, 
+                                                                    {'lastAct' : 'seg_jobTitle'}
+                                                                ]})
+wt.segWord('jouThaiUsr01','jobpass','jobThai','jobList' ,'jobRequire'
+                                                        ,'jobRequireWord'
+                                                        ,{"$or":[   {'lastAct' : 'newAdded'}, 
+                                                                    {'lastAct' : 'seg_jobTitle'},
+                                                                    {'lastAct' : 'seg_jobDetail'}
+                                                                ]})
 
 
 
-# wdCount = WordCount('jouThaiUsr01','jobpass','jobThai','jobDetailWord','jobDetailWordCount',"wordJobDetail",'seqNum')
-# wdCount.counting()
+wdCount = WordCount('jouThaiUsr01','jobpass','jobThai','jobDetailWord','jobDetailWordCount',"wordJobDetail",'seqNum')
+wdCount.counting()
